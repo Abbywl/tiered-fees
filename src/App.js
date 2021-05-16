a3288f3181b0adb357b43dbec628a198c0ecf92f
Abbywl <45375909+Abbywl@users.noreply.github.com>
2021-05-16 09:04:21 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,241 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Container from '@material-ui/core/Container';
+import { TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+import { Typography } from '@material-ui/core';
+import{ makeStyles } from '@material-ui/core/styles';
+import { calculateFees } from './Calculate.mjs';
+import { areTiersValid } from './Calculate.mjs';
+import { dollarInputValid } from './Calculate.mjs';
 
+document.body.style = 'background: #DDD1FF;';
+//Just Material UI formatting
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+      margin: theme.spacing(2),
+      },
+    },
+    button: {
+      margin: theme.spacing(2),
+    }
+}))
+
+//Actual App function starts here
 function App() {
+
+  const classes = useStyles()
+  const [inputFields, setInputFields] = useState(
+    [
+      {lowerBound: '', upperBound: 'INF', fees: '0.00'},
+    ]);
+  const [price, setPrice] = useState('0.00');
+
+  const handleSubmitTiers = (event) => {
+        event.preventDefault();
+        console.log(areTiersValid([...inputFields]).message);
+  };
+
+  //handles change of single input box
+  const handleChangeInput = (index, event) => {
+    const values = [...inputFields];
+    if(dollarInputValid(event.target.value))
+    {
+          values[index][event.target.name] = event.target.value;
+          setInputFields(values);
+    }
+  }
+
+  const handleAddTiers = () => {
+    if(inputFields.length === 1) //what if length === 0?
+    {
+      const ogVals = [...inputFields];
+      const changedVal = [{lowerBound: ogVals[0].lowerBound, upperBound: '', fees: ogVals[0].fees}];
+      //changed the upperBound of the single tier to empty string instead of INF
+      const newVals = changedVal.concat([{lowerBound: '', upperBound: "INF", fees: '0.00'}]);
+      setInputFields(newVals);
+    }
+    else{
+    const values = inputFields.slice(0,[...inputFields].length -1);//array without last element
+    const lastVal = inputFields[inputFields.length-1];//last value
+    const newVals = values.concat([{lowerBound: lastVal.lowerBound, upperBound: '', fees: lastVal.fees}]);
+
+    setInputFields(newVals.concat([{lowerBound: '', upperBound: "INF", fees: '0.00'}]));
+    }
+  }
+
+  const handleRemoveTiers = () => {
+    if(inputFields.length > 2)
+    {
+      const values = inputFields.slice(0,[...inputFields].length -2);//array without last 2 element
+      const lastVal = inputFields[inputFields.length-2];//2nd to last element
+      const newVal = [{lowerBound: lastVal.lowerBound, upperBound: "INF", fees: lastVal.fees}];
+      //newVal is new last element with INF upperBound
+      setInputFields(values.concat(newVal));
+    }
+    else if(inputFields.length === 2)
+    {
+      const values = [...inputFields][0];
+      setInputFields([{lowerBound: values.lowerBound, upperBound: "INF", fees: values.fees}]);
+    }
+  }
+
+  const handleSetPrice = (event) => {
+    if(dollarInputValid(event.target.value))
+     { setPrice(event.target.value); }
+  }
+
+  const handleCalculateFees = (event) => {
+    const tierStatus = areTiersValid([...inputFields]);
+    if(tierStatus.valid)
+    {
+      console.log(calculateFees( [...inputFields], price));
+    }
+  }
+  
+
   return (
-    <div className="App">
+    <Container>
+      <Typography variant="h4" component="h1" color="primary">
+          Tiered Fees
+      </Typography>
+      <Typography variant="subtitle2" component="h2" color="primary" align= 'justify'>
+          Please enter values in dollar amounts. e.g. 1.00 for a dollar
+      </Typography>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className = {classes.root} onSubmit={handleSubmitTiers} >
+          { inputFields.slice(0,[...inputFields].length -1).map((inputField, index) => (
+            <div key={index}>
+              {/*<input type="number" step = "0.01" min="0" />*/}
+
+              <TextField
+                name="lowerBound"
+                label="Lower Bound"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                variant="outlined"
+                type="number"
+                value={inputField.lowerBound}
+                onChange={event => handleChangeInput(index, event) }
+                />
+              <TextField
+                name="upperBound"
+                label="Upper Bound"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                variant="outlined"
+                type="number"
+                value={inputField.upperBound}
+                onChange={event => handleChangeInput(index, event) }     
+                />
+                <TextField
+                name="fees"
+                label="Fees"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                variant="outlined"
+                type="number"
+                value={inputField.fees}
+                onChange={event => handleChangeInput(index, event) }
+                />
+
+            </div>
+          ))
+          }
+             
+             { inputFields.slice([...inputFields].length -1).map((inputField, index) => (
+            <div key={[...inputFields].length-1 }>
+              {/*<input type="number" step = "0.01" min="0" />*/}
+
+              <TextField
+                name="lowerBound"
+                label="Lower Bound"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                variant="outlined"
+                type="number"
+                value={inputField.lowerBound}
+                onChange={event => handleChangeInput([...inputFields].length-1, event) }
+                />
+              <TextField
+                disabled
+                name="upperBound"
+                label="Upper Bound"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                variant="outlined"
+                value={inputField.upperBound}
+                onChange={event => handleChangeInput([...inputFields].length-1, event) }     
+                />
+                <TextField
+                name="fees"
+                label="Fees"
+                InputLabelProps={{shrink: true}}
+                variant="outlined"
+                type="number"
+                value={inputField.fees}
+                onChange={event => handleChangeInput([...inputFields].length-1, event) }
+                />
+
+            </div>
+          ))
+          }
+           
+          <Button 
+          className= {classes.button}
+          variant= "contained"
+          color = "primary"
+          type = "submit" 
+          endIcon={<Icon>autorenew</Icon>}
+          onClick={handleSubmitTiers}>
+          Submit Tiers
+        </Button>
+          <IconButton
+            onClick = { () => handleRemoveTiers() }
+          >
+                <RemoveIcon/>
+          </IconButton>
+          <IconButton
+            onClick = { () => handleAddTiers() }
+            >
+                <AddIcon/>
+          </IconButton>
+          <p></p>
+          <TextField
+                name="price"
+                label="Price"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                variant="outlined"
+                type="number"
+                value={price}
+                onChange={event => handleSetPrice(event) }
+          />
+
+          <Button 
+          className= {classes.button}
+          variant= "contained"
+          color = "primary"
+          type = "submit" 
+          onClick={handleCalculateFees}>
+          Calculate Fees
+        </Button>
+
+        </form>
       </header>
-    </div>
+      
+    </Container>
   );
 }
 
---
+++ src/Calculate.mjs
@@ -0,0 +1,120 @@
+//calculation helper functions
+
+export function dollarInputValid(amount)
+{ //returns bool based on if the dollar input amount is valid
+  if(amount >= 0)
+    { 
+      const amountStrArr = amount.toString().split(".");
+      if( amountStrArr.length > 1 && amountStrArr[1].length > 2)
+      {
+        console.log("dollar amount must be to 2 decimal places.");
+        return false;
+      }
+      else { return true; }
+    }
+    else { 
+      console.log("price must be positive");
+      return false;
+    }
+}
+
+function toCents(dollars)
+{
+  //console.log("parseFloat(dollars) " + parseFloat(dollars));
+  //console.log("parseFloat(dollars) * 100)" + parseFloat(dollars) * 100);
+  //console.log("Math.floor(parseFloat(dollars) * 100)" + Math.floor(parseFloat(dollars) * 100));
+  const dollarArr = dollars.toString().split(".");
+  var dollarCents = 0;
+  var cents = 0;
+  if(dollarArr.length > 1)
+  {
+    if(dollarArr[0] === "")
+    { dollarCents = 0;}
+    else { dollarCents = parseInt(dollarArr[0]) * 100; }
+
+    if(dollarArr[1] === "")
+    { cents = 0;}
+    else { cents = parseInt(dollarArr[1]); }
+    console.log("dollars: " + dollars);
+    console.log("dollarCents: " + dollarCents);
+    console.log("cents: " + cents);
+    return dollarCents + cents;
+  }
+  return Math.floor(parseFloat(dollars) * 100);
+}
+
+function adjustTierFee(fees)
+{ //formats the fee input for printing to the console
+  if(fees === "")
+      { return "None"; }
+  else { return "$" + parseFloat(fees).toFixed(2).toString() ; }
+}
+
+export function calculateFees(values, p)
+{ //returns string containing the fee amount or error message
+  //called by handleCalculateFees
+  if(p === "")
+  {
+    return "No price is provided. Please provide a price.";
+  }
+  const price = toCents(p);
+  if(values.length === 1)
+  {
+    const tier = values[0];
+    if(price >= toCents(tier.lowerBound))
+    {  return "FEE: " + adjustTierFee(tier.fees); }
+  }
+  if(price < toCents(values[0].lowerBound))
+  {
+      return "Price provided is below the lowest price bound. Please provide a value above the lowest price bound.";
+  }
+  
+  for(let i = 0; i < values.length-1; i++)
+  {
+    const tier = values[i];
+    if(price >= toCents(tier.lowerBound) && price <= toCents(tier.upperBound))
+    {
+      return "FEE: " + adjustTierFee(tier.fees);
+    }
+  }
+  const topTier = values[values.length - 1];
+  return "FEE: " + adjustTierFee(topTier.fees);
+}
+
+export function areTiersValid(values)
+{//checks if user inputted tier combination is valid
+  //returns a message for printing to the console, and a boolean value
+  if(values.length === 1)
+  {
+    const tier = values[0];
+    if(tier.lowerBound === "")
+    { return {message: "Tier is not valid. Please provide a Lower Bound value", valid: false}; }
+  }
+  for(let i = 0; i < values.length-1; i++)
+  {
+    const tier = values[i];
+    const nextTier = values[i+1];
+    if(tier.lowerBound === ""  || tier.upperBound === "" || nextTier.lowerBound === "" )
+    {
+        return {message: "Tiers are not valid. Please make sure all tiers have values provided", valid: false};
+    }
+    else if(toCents(tier.upperBound) !==  toCents(nextTier.lowerBound)- 1) 
+    {
+      console.log( "lower tier upper bound: " + toCents(tier.upperBound));
+      console.log( "upper tier lower bound: " + toCents(nextTier.lowerBound));
+      console.log("upper tier lower bound minus one  " + (toCents(nextTier.lowerBound)- 1));
+
+      const m = "Tiers are not valid. Tier " + i + " has upper bound " + tier.upperBound +
+      " while Tier " + (i+1) + " has lower bound " + nextTier.lowerBound + 
+      ". They must have a difference of .01 to be valid.";
+        return {message: m, valid: false};
+    }  
+    else if(toCents(tier.lowerBound) > toCents(tier.upperBound))
+    {
+      const m = "Tiers are not valid. Tier " + i + " has lower bound " + tier.lowerBound +
+      " and upper bound " + tier.upperBound;
+        return {message: m, valid: false};
+    }
+  }
+    return { message: "Tiers are valid.", valid: true};
+}
